e00f15e28f65b6b1033270044d6adab242a4dda6
Shaidhms <mail2shaid@gmail.com>
2026-01-09 18:27:56 +0530

    Fix: Import useMemo in ChecklistSection

--- src/components/ChecklistSection.jsx
+++ src/components/ChecklistSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Plus, Sparkles } from 'lucide-react';
 import { checklistService } from '../services/api';
 import { CHECKLIST_CATEGORIES } from '../utils/constants';
